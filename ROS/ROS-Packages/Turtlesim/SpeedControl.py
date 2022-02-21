e651d73e516defb2d9553255fd711e9c133c5e2a
Doha ElHady <86476979+DohaElHady@users.noreply.github.com>
2022-02-21 17:04:00 +0200

    Update SpeedControl.py

--- ROS/ROS-Packages/Turtlesim/SpeedControl.py
+++ ROS/ROS-Packages/Turtlesim/SpeedControl.py
@@ -2,23 +2,28 @@
 import rospy
 from geometry_msgs.msg import Twist
 
-
 rospy.init_node('velocity_teller',anonymous=True)
 velocity_send=rospy.Publisher('/turtle1/cmd_vel',Twist,queue_size=10)
 rate=rospy.Rate(10)
 velocity_msg= Twist()
 
 while not rospy.is_shutdown():
-  
     speed = int(input("Input your speed:"))
     distance_target = int (input("Type your distance:"))
-
-    velocity_msg.linear.x=speed
-    velocity_msg.linear.y=0
-    velocity_msg.linear.z=0
+    direction= input("Type your direction:")
     velocity_msg.angular.x=0
     velocity_msg.angular.y=0
     velocity_msg.angular.z=0
+    velocity_msg.linear.x=0
+    velocity_msg.linear.y=0
+    velocity_msg.linear.z=0
+
+    if direction=='x':
+        velocity_msg.linear.x=speed
+    elif direction=='y':
+        velocity_msg.linear.y=speed
+    elif direction=='z':
+        velocity_msg.linear.z=speed
 
     t0= rospy.Time.now().to_sec()
     distance_moved=0
@@ -28,6 +33,9 @@ while not rospy.is_shutdown():
         t1=rospy.Time.now().to_sec()
         distance_moved= abs(speed)*(t1-t0)
 
+
     velocity_msg.linear.x=0
+    velocity_msg.linear.y=0
+    velocity_msg.linear.z=0
     velocity_send.publish(velocity_msg)
     rate.sleep()
