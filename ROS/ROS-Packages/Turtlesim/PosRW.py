7596e812284f247505e2cb41878cf2638f318c37
Doha ElHady <86476979+DohaElHady@users.noreply.github.com>
2022-02-21 20:46:05 +0200

    Update PosRW.py

--- ROS/ROS-Packages/Turtlesim/PosRW.py
+++ ROS/ROS-Packages/Turtlesim/PosRW.py
@@ -4,13 +4,7 @@ from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 
 ###############################################################################
-'''
-The ROS python library runs additional threads to handle message subscription and publishing.
-So when you call rospy.Subscribe in your code, it doesn't check for incoming messages and
-execute callbacks itself. It simply adds the topic and function to a list.
-This list is regularly checked in a different thread which executes the callbacks as needed.
-This is why you don't need any polling in your code to make the message callbacks work and you need to add your looping code in it.
-'''
+
 def velocity_sender():
         speed = int(input("Input your speed:"))
         distance_target = int (input("Type your distance:"))
@@ -44,6 +38,14 @@ def velocity_sender():
 
 
 ###############################################################################
+
+'''
+The ROS python library runs additional threads to handle message subscription and publishing.
+So when you call rospy.Subscribe in your code, it doesn't check for incoming messages and
+execute callbacks itself. It simply adds the topic and function to a list.
+This list is regularly checked in a different thread which executes the callbacks as needed.
+This is why you don't need any polling in your code to make the message callbacks work and you need to add your looping code in it.
+'''
 
 def pos_recieve_action(message):
     rospy.loginfo("recieved({})".format(message))
